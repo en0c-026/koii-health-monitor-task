c062f846652a5521005c42eb853d51bb13841dbb
Samsung <bitup.games@gmail.com>
2023-06-01 19:49:08 +0200

    fix: getTaskNodes helper

--- helpers/getTaskNodes.js
+++ helpers/getTaskNodes.js
@@ -10,7 +10,6 @@ async function getNodesByTaskId(taskId) {
     const nodePublicKeys = res.data.map((node) => (node.submitterPubkey))
     return { taskId, nodePublicKeys, runningNodes: nodePublicKeys.length }
   } catch (error) {
-    console.log('error on getNodesByTaskId', error)
     return [];
   }
 }
@@ -21,26 +20,12 @@ async function getTaskNodes(connection) {
   for (let i = 0; i < taskIds.length; i++) {
     const taskId = taskIds[i];
     const nodesData = await getNodesByTaskId(taskId)
-    allNodes.push(nodesData)
+    if (nodesData.length > 0) {
+      allNodes.push(nodesData)
+    }
     await new Promise((resolve) => setTimeout(resolve, 250))
   }
   return allNodes
-
-  // const allNodesUrlsFlat = allNodesUrlsRaw.flat();
-  // const allNodesUrlsUnique = [...new Set(allNodesUrlsFlat)];
-
-  // let taskNodes = []
-  // for (let i = 0; i < allNodesUrlsUnique.length; i++) {
-  //   const url = allNodesUrlsUnique[i];
-  //   console.log(url);
-  //   const response = await getIp(url)
-  //   console.log(response);
-  //   taskNodes.push(response)
-  //   await new Promise((resolve) => setTimeout(resolve, 250))
-
-  // }
-
-  // return taskNodes
 
 }
 
