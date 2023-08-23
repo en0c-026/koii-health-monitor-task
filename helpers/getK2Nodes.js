bd8d9622d2eb4393e00457e8c60747ae453516a0
en0c-026 <bitup.games@gmail.com>
2023-08-23 19:42:48 +0200

    update: Geo2IP service URL

--- helpers/getK2Nodes.js
+++ helpers/getK2Nodes.js
@@ -1,14 +1,14 @@
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 const net = require('net');
 const { default: axios } = require('axios');
-const GEO2IP_URL = "https://geoip.maxmind.com/geoip/v2.1/country/"
+const iso3166 = require("iso-3166-1")
+
+const GEO2IP_URL = process.env.SECRET_GEO2IP_ACCOUNT.GEO2IP_URL
 const GEO2IP_ACCOUNT = process.env.SECRET_GEO2IP_ACCOUNT
 const GEO2IP_KEY = process.env.SECRET_GEO2IP_KEY
-const iso3166 = require("iso-3166-1")
-async function geolocalizate(ip) {
 
-  if (!GEO2IP_ACCOUNT || !GEO2IP_KEY) {
+async function geolocalizate(ip) {
+  if (!GEO2IP_URL || !GEO2IP_ACCOUNT || !GEO2IP_KEY) {
     throw new Error("env vars GEO2IP_ACCOUNT or GEO2IP_KEY undefined")
   }
   const credentials = Buffer.from(`${GEO2IP_ACCOUNT}:${GEO2IP_KEY}`).toString('base64');
@@ -39,41 +39,44 @@ async function measureResponseTime(node) {
 }
 
 async function getK2Nodes(connection) {
-  const nodes = await connection.getClusterNodes()
+  try {
+    const nodes = await connection.getClusterNodes()
+    let formattedData = []
+    for (let i = 0; i < nodes.length; i++) {
+      const node = nodes[i];
 
-  let formattedData = []
-  for (let i = 0; i < nodes.length; i++) {
-    const node = nodes[i];
+      if (node.rpc) {
+        const [host, port] = node.rpc.split(':')
+        const country = (await geolocalizate(host)).country.iso_code
+        const country_alpha3 = iso3166.whereAlpha2(country).alpha3
+        const topology = await measureResponseTime({ host, port })
+        formattedData[i] = {
+          pubkey: node.pubkey,
+          ip: host,
+          country,
+          country_alpha3,
+          ...topology
+        }
+      } else {
+        const [host] = node.gossip.split(':')
+        const country = (await geolocalizate(host)).country.iso_code
 
-    if (node.rpc) {
-      const [host, port] = node.rpc.split(':')
-      const country = (await geolocalizate(host)).country.iso_code
-      const country_alpha3 = iso3166.whereAlpha2(country).alpha3
-      const topology = await measureResponseTime({ host, port })
-      formattedData[i] = {
-        pubkey: node.pubkey,
-        ip: host,
-        country,
-        country_alpha3,
-        ...topology
+        formattedData[i] = {
+          pubkey: node.pubkey,
+          ip: host,
+          country,
+          alive: false,
+          responseTime: null
+        }
       }
-    } else {
-      const [host] = node.gossip.split(':')
-      const country = (await geolocalizate(host)).country.iso_code
+      await new Promise((resolve) => setTimeout(resolve, 500))
 
-      formattedData[i] = {
-        pubkey: node.pubkey,
-        ip: host,
-        country,
-        alive: false,
-        responseTime: null
-      }
     }
-    await new Promise((resolve) => setTimeout(resolve, 500))
 
+    return formattedData
+  } catch (error) {
+    console.log(error);
   }
-
-  return formattedData
 }
 
 module.exports = getK2Nodes;
